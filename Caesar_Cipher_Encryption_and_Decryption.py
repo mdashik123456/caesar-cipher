cfd0cef98d9f04a70d68f62dee8ac14e4e3c5fc5
Mohammad Ashiqur Rahman <39029566+mdashik123456@users.noreply.github.com>
2022-04-04 19:09:31 +0600

    Update Caesar_Cipher_Encryption_and_Decryption.py

--- Caesar_Cipher_Encryption_and_Decryption.py
+++ Caesar_Cipher_Encryption_and_Decryption.py
@@ -1,33 +1,30 @@
 from other import logo
 from other import alphabet
 
-print(logo)
-
-def cipher_text(text, n, choice):
+def cipher_text(text, shift, choice):
     cipher = ""
     if choice == "decode":
-        n *= -1
+        shift *= -1
     for char in text:
         if char in alphabet:
             position = alphabet.index(char)
-            cipher += alphabet[position + n]
+            cipher += alphabet[position + shift]
         else:
             cipher += char
-    if choice != "encode" and choice != "decode":
-        print("\nWrong Selection\nYou need to type 'encode' to encrypt or 'decode' to decrypt\n")
-        return
     print(f"The {choice}d text is : \"{cipher}\"")
+    
+    
+print(logo)
+while True:
+    choice = input("Type 'encode' to encrypt\nType 'decode' to decrypt\n==> ")
+    if choice == "encode" or choice == "decode":
+        text = input("Type your messege : ")
+        shift = int(input("Type the shift number : "))
+        shift = shift % int((len(alphabet) / 2))
+        cipher_text(text, shift, choice)
+    else:
+        print("\nWrong Selection\nYou need to type 'encode' to encrypt or 'decode' to decrypt\n")
 
-conti = 'y'
-while conti == 'y':
-    
-    choice = input("Type 'encode' to encrypt\nType 'decode' to decrypt\n")
-    
-    text = input("Type your messege : ")
-    n = int(input("Type the shift number : "))
-    n = n % int((len(alphabet) / 2))
-    cipher_text(text, n, choice)
-    
-    conti = input("Do you want to continue? (Y/N) : ")
-    conti = conti.lower()
+    isContinue = input("Do you want to continue? (Y/N) : ")
+    if isContinue == 'n' or isContinue == 'N': break
 print("\nGoodbye\n")
