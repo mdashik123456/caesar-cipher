0842e49334105f050a98e03a253fe43db4197fd9
Mohammad Ashiqur Rahman <39029566+mdashik123456@users.noreply.github.com>
2023-08-21 15:15:42 -0400

    Add files via upload

--- Caesar_Cipher_Encryption_and_Decryption.py
+++ Caesar_Cipher_Encryption_and_Decryption.py
@@ -1,30 +1,62 @@
-from other import logo
-from other import alphabet
+from other import *
+from os import system
+import platform
 
-def cipher_text(text, shift, choice):
-    cipher = ""
-    if choice == "decode":
-        shift *= -1
+def clear_display():
+    if platform.system() == "Windows":
+        system("cls")
+    else:
+        system("clear")
+
+def encode(text, key):
+    cipher_text = ""
+    
     for char in text:
-        if char in alphabet:
-            position = alphabet.index(char)
-            cipher += alphabet[position + shift]
-        else:
-            cipher += char
-    print(f"The {choice}d text is : \"{cipher}\"")
+        position = alphabet.index(char) + 1
+        cipher_text_i = position + key
+        if cipher_text_i > alphabet_len:
+            cipher_text_i = cipher_text_i % alphabet_len
+        cipher_text += alphabet[cipher_text_i - 1]
+    print(f"The {choice}d text is : \"{cipher_text}\"")
     
+
+def decode(text, key):
+    plain_text = ""
     
-print(logo)
+    for char in text:
+        position = alphabet.index(char) + 1
+        plain_text_i = position - key
+        if plain_text_i < 1:
+            plain_text_i = plain_text_i + (alphabet_len * (int((plain_text_i*(-1)) / alphabet_len)+1))
+        plain_text += alphabet[plain_text_i - 1]
+    print(f"The {choice}d text is : \"{plain_text}\"")
+
+
+
+#Program start from here
+global alphabet_len
+alphabet_len = len(alphabet)
+
+clear_display()
+    
 while True:
+    print(logo)
     choice = input("Type 'encode' to encrypt\nType 'decode' to decrypt\n==> ")
     if choice == "encode" or choice == "decode":
         text = input("Type your messege : ")
-        shift = int(input("Type the shift number : "))
-        shift = shift % int((len(alphabet) / 2))
-        cipher_text(text, shift, choice)
+        key = int(input("Type the key number : "))
+        # key = key % int((len(alphabet) / 2))
+        if choice == "encode":
+            encode(text, key)
+        else:
+            decode(text, key)
     else:
         print("\nWrong Selection\nYou need to type 'encode' to encrypt or 'decode' to decrypt\n")
 
     isContinue = input("Do you want to continue? (Y/N) : ")
+    clear_display()
     if isContinue == 'n' or isContinue == 'N': break
-print("\nGoodbye\n")
+    
+print("\n\n---------------------------------------------------")
+print("\nGoodbye\nThanks for using....")
+print("---------------------------------------------------")
